a3493148a8caace416a910b699f10286512bb64d
Dayan Monteiro <dmsmamb@gmail.com>
2021-05-21 15:08:51 -0300

    interpolação de string

--- data-binding-app/src/app/app.module.ts
+++ data-binding-app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringInterpolationComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
